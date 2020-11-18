f350292e07edd7687433a70b7e52871059cba524
Linfeng Z <33096195+lz346@users.noreply.github.com>
2020-11-17 22:45:37 -0500

    Initial commit

---
+++ src/main/java/com/example/practice/PracticetestApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticetestApplication.class, args);
+	}
+
+}
